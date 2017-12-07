78112827d9a16cbf228bf3c95a8dd5b7a43c814e
AntonMelekhavets <antohage@gmail.com>
2017-12-07 14:29:33 +0300

    Created WinForms

---
+++ DeviceView.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace DeviceManager
+{
+    partial class DeviceView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.devicesList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // devicesList
+            // 
+            this.devicesList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.devicesList.Location = new System.Drawing.Point(13, 12);
+            this.devicesList.Name = "devicesList";
+            this.devicesList.Size = new System.Drawing.Size(1182, 460);
+            this.devicesList.TabIndex = 1;
+            this.devicesList.UseCompatibleStateImageBehavior = false;
+            this.devicesList.View = System.Windows.Forms.View.Details;
+            this.devicesList.SelectedIndexChanged += new System.EventHandler(this.devicesList_SelectedIndexChanged);
+            this.devicesList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.DisableEnableDevice);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Device Description";
+            this.columnHeader1.Width = 197;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Enabled";
+            this.columnHeader2.Width = 53;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "GUID";
+            this.columnHeader3.Width = 108;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Device Path";
+            this.columnHeader4.Width = 150;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "HardwareID";
+            this.columnHeader5.Width = 166;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Manufacturer";
+            this.columnHeader6.Width = 165;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Driver Description";
+            this.columnHeader7.Width = 176;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Driver Path";
+            this.columnHeader8.Width = 163;
+            // 
+            // DeviceView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1207, 484);
+            this.Controls.Add(this.devicesList);
+            this.Name = "DeviceView";
+            this.Text = "Device Manager";
+            this.Load += new System.EventHandler(this.LoadForm);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView devicesList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+    }
+}
+
---
+++ DeviceView.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace DeviceManager
+{
+    public partial class DeviceView : Form
+    {
+        private static readonly DeviceSearcher Searcher = new DeviceSearcher();
+        private List<Device> _deviceList;
+
+        public DeviceView()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadForm(object sender, EventArgs e)
+        {
+            _deviceList = new List<Device>();
+            CreateForm();
+        }
+        private void CreateForm()
+        {
+            devicesList.Items.Clear();
+            _deviceList = Searcher.GetDevices();
+
+            foreach (var device in _deviceList)
+            {
+                var deviceInfo = new ListViewItem(device.DeviceDescription);
+                deviceInfo.SubItems.AddRange(new[]
+                {
+                    device.Status.ToString(),
+                    device.Guid,
+                    device.DevicePath,
+                    device.HardwareId,
+                    device.Manufacturer,
+                    device.DriverDescription,
+                    device.DriverPath
+                });
+                devicesList.Items.Add(deviceInfo);
+            }
+        }
+
+        private void DisableEnableDevice(object sender, MouseEventArgs e)
+        {
+            var item = (sender as ListView).HitTest(e.Location).Item.SubItems;
+            var device = _deviceList[devicesList.HitTest(e.Location).Item.Index];            
+            if (device.Status)
+            {
+                device.DisEnable("Disable");
+                MessageBox.Show("Disable complete", "Status", MessageBoxButtons.OK);
+                device.Status = false;
+                item[1].Text = false.ToString();
+            }
+            else
+            {
+                device.DisEnable("Enable");
+                MessageBox.Show("Enable complete", "Status", MessageBoxButtons.OK);
+                device.Status = true;
+                item[1].Text = true.ToString();
+            }
+        }
+
+        private void devicesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
