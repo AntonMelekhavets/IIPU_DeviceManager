49736aa95ed4785fe7cdf3a19958ce888e9bdb60
AntonMelekhavets <antohage@gmail.com>
2017-12-07 15:31:12 +0300

    Added description of device

---
+++ Device.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Management;
+
+
+namespace DeviceManager
+{
+    class Device
+    {
+        public string DevicePath { get; set; }
+
+        public string DeviceDescription { get; set; }
+
+        public string Guid { get; set; }
+
+        public string HardwareId { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string DriverDescription { get; set; }
+
+        public string DriverPath { get; set; }
+
+        public bool Status { get; set; }
+        
+    }
+}
